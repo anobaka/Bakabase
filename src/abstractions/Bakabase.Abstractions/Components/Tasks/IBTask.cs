07021516cb0a2dabe397127c5222deff492f90a2
anobaka <anobaka@qq.com>
2025-03-18 23:02:56 +0800

    update

---
+++ src/abstractions/Bakabase.Abstractions/Components/Tasks/IBTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Bakabase.Abstractions.Components.Tasks;
+
+public interface IBTask
+{
+    string Key { get; }
+    CancellationToken? Ct { get; }
+    string Name { get; }
+    string Description { get; }
+
+    Task ChangeInterval(TimeSpan interval);
+    TimeSpan Interval { get; }
+    Task Start();
+    Task Stop();
+    Task Disable();
+    Task Enable();
+}
