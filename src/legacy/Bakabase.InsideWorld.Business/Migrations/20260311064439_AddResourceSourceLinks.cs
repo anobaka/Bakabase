8510e31f19da890ae28142deb95ced79d5768d45
anobaka <anobaka@qq.com>
2026-03-11 15:38:38 +0800

    chore: add migration

--- src/legacy/Bakabase.InsideWorld.Business/Migrations/20260311064439_AddResourceSourceLinks.cs
+++ src/legacy/Bakabase.InsideWorld.Business/Migrations/20260311064439_AddResourceSourceLinks.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -5,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Bakabase.InsideWorld.Business.Migrations
 {
     /// <inheritdoc />
-    public partial class RemoveSourceAndSourceKeyFromResource : Migration
+    public partial class AddResourceSourceLinks : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -25,11 +26,46 @@ namespace Bakabase.InsideWorld.Business.Migrations
             migrationBuilder.DropColumn(
                 name: "SourceKey",
                 table: "ResourcesV2");
+
+            migrationBuilder.CreateTable(
+                name: "ResourceSourceLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ResourceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Source = table.Column<int>(type: "INTEGER", nullable: false),
+                    SourceKey = table.Column<string>(type: "TEXT", nullable: false),
+                    CreateDt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceSourceLinks", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceSourceLinks_ResourceId",
+                table: "ResourceSourceLinks",
+                column: "ResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceSourceLinks_ResourceId_Source_SourceKey",
+                table: "ResourceSourceLinks",
+                columns: new[] { "ResourceId", "Source", "SourceKey" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceSourceLinks_Source_SourceKey",
+                table: "ResourceSourceLinks",
+                columns: new[] { "Source", "SourceKey" });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "ResourceSourceLinks");
+
             migrationBuilder.AddColumn<int>(
                 name: "Source",
                 table: "ResourcesV2",
