8280929cf1e8cc88797a76b5d92cb044dfb77fc8
anobaka <anobaka@qq.com>
2026-03-25 13:46:18 +0800

    chore: delete deprecated pages

--- src/web/src/pages/deprecated/media-library-template/components/PathPropertyExtractor/index.ts
+++
@@ -1,4 +0,0 @@
-import PathPropertyExtractorDemonstrator from "./Demonstrator.tsx";
-import PathPropertyExtractorModal from "./Modal.tsx";
-
-export { PathPropertyExtractorDemonstrator, PathPropertyExtractorModal };
