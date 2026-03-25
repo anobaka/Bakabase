2e95042383e9c216e5089acf8314f0853f6897e7
anobaka <anobaka@qq.com>
2026-03-25 16:31:43 +0800

    chore: create ef migrations

---
+++ src/legacy/Bakabase.InsideWorld.Business/Migrations/20260325083120_CleanupDeprecatedModels.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bakabase.InsideWorld.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanupDeprecatedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryCustomPropertyMappings");
+
+            migrationBuilder.DropTable(
+                name: "ComponentOptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryCustomPropertyMappings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Order = table.Column<int>(type: "INTEGER", nullable: false),
+                    PropertyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryCustomPropertyMappings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComponentOptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ComponentAssemblyQualifiedTypeName = table.Column<string>(type: "TEXT", nullable: false),
+                    ComponentType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Json = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComponentOptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryCustomPropertyMappings_CategoryId_PropertyId",
+                table: "CategoryCustomPropertyMappings",
+                columns: new[] { "CategoryId", "PropertyId" },
+                unique: true);
+        }
+    }
+}
